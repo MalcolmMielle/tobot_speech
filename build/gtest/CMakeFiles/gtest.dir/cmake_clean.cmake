4b3bda8743224a4f47ada1fc1e0bee113d6bfcef
Malcolm Mielle <ros@ros-K56CM.(none)>
2014-04-13 01:44:35 +0800

    first commit

---
+++ build/gtest/CMakeFiles/gtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  "/home/ros/catkin_ws/src/Tobot/tobot_speech/lib/libgtest.pdb"
+  "/home/ros/catkin_ws/src/Tobot/tobot_speech/lib/libgtest.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
